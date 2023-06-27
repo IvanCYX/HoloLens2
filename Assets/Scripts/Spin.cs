aaaac3dfb9deee20807a78319082221076b7b1f2
Ivan Cheah <69534752+IvanCYX@users.noreply.github.com>
2023-06-27 14:13:24 -0500

    Scripts
    Script to rotate servo motor
    Script to rotate cube

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    private float rotationSpeed = 100f;
+    private float delay = 0.015f;
+    private bool isRotating = false;
+
+    private void Start()
+    {
+    }
+
+    private System.Collections.IEnumerator RotateCube()
+    {
+        // Rotate the cube 180 degrees on the y-axis
+        Quaternion targetRotation = Quaternion.Euler(0f, 180f, 0f);
+        Quaternion initialRotation = transform.rotation;
+
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * (rotationSpeed / 180f);
+            transform.rotation = Quaternion.Lerp(initialRotation, targetRotation, t);
+            yield return null;
+        }
+
+        // Wait for the specified delay
+        yield return new WaitForSeconds(delay);
+
+        // Rotate the cube back 180 degrees on the y-axis
+        targetRotation = Quaternion.Euler(0f, 0f, 0f);
+        initialRotation = transform.rotation;
+
+        t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * (rotationSpeed / 180f);
+            transform.rotation = Quaternion.Lerp(initialRotation, targetRotation, t);
+            yield return null;
+        }
+
+        // Rotation completed
+        isRotating = false;
+    }
+
+    public void startSpin()
+    {
+        StartCoroutine(RotateCube());
+    }
+}
