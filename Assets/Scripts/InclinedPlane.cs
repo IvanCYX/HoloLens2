721fffdde3947fee02419355dd20c82b7d4ba156
Ivan Cheah <69534752+IvanCYX@users.noreply.github.com>
2023-07-25 13:47:40 -0500

    Updated InclinedPlane

--- Assets/Scripts/InclinedPlane.cs
+++ Assets/Scripts/InclinedPlane.cs
@@ -2,48 +2,55 @@ using UnityEngine;
 
 public class InclinedPlane : MonoBehaviour
 {
-    public float initialVelocity = 5f;
-    public float displacement = 10f;
-    public float acceleration = 1f;
-    //angle along z-axis
-    public float rampAngle = 30f;
-    public float frictionCoefficient = 0.1f;
+    public float rampAngle = 30f;         // Angle of the ramp in degrees
+    public float frictionCoefficient = 0.1f;  // Friction coefficient
+    public float initialVelocity = 5f;   // Initial velocity of the block
 
-    private Rigidbody rb;
-    private float normalForce;
-    private float frictionForce;
+    private Vector3 inclinedDirection;   // Direction of the inclined plane
+    private float accelerationAlongRamp; // Acceleration of the block along the inclined plane
+    private float normalForce;           // Normal force acting on the block
+    private float frictionForce;         // Frictional force acting on the block
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        MoveBlock();
-    }
+        // Calculate the direction of the inclined plane (ignoring Z component)
+        inclinedDirection = Quaternion.Euler(-rampAngle, 0f, 0f) * Vector3.forward;
+        inclinedDirection.z = 0f;
+        inclinedDirection.Normalize();
 
-    private void MoveBlock()
-    {
-        // Calculate normal force
-        normalForce = rb.mass * Mathf.Cos(rampAngle * Mathf.Deg2Rad);
+        // Calculate the acceleration of the block along the inclined plane
+        accelerationAlongRamp = Physics.gravity.magnitude * Mathf.Sin(rampAngle * Mathf.Deg2Rad) - frictionCoefficient * Physics.gravity.magnitude * Mathf.Cos(rampAngle * Mathf.Deg2Rad);
+        Debug.Log(accelerationAlongRamp);
+
+        // Calculate the normal force acting on the block
+        normalForce = GetComponent<Rigidbody>().mass * Mathf.Cos(rampAngle * Mathf.Deg2Rad);
         Debug.Log(normalForce);
 
-        // Calculate frictional force
+        // Calculate the frictional force acting on the block
         frictionForce = frictionCoefficient * normalForce;
-        Debug.Log(frictionForce);
 
-        // Calculate acceleration of block along inclined plane
-        float rampAngleRad = rampAngle * Mathf.Deg2Rad;
-        float accelerationAlongRamp = acceleration * Mathf.Sin(rampAngleRad);
+        // Set the initial velocity of the block along the inclined plane
+        Vector3 initialVelocityVector = inclinedDirection * initialVelocity;
+        Debug.Log(initialVelocityVector);
+        GetComponent<Rigidbody>().velocity = initialVelocityVector;
+    }
 
-        // Adjust acceleration to account for friction
-        accelerationAlongRamp -= frictionForce / rb.mass;
+    private void Update()
+    {
+        // Calculate the current velocity vector along the inclined plane
+        Vector3 currentVelocity = GetComponent<Rigidbody>().velocity;
+        Vector3 currentVelocityAlongRamp = Vector3.Dot(currentVelocity, inclinedDirection) * inclinedDirection;
 
-        // Calculate time taken for block to reach displacement
-        float time = Mathf.Sqrt(2f * displacement / accelerationAlongRamp);
+        // Calculate the acceleration of the block along the inclined plane
+        Vector3 acceleration = accelerationAlongRamp * inclinedDirection;
 
-        // Calculate final velocity at end of displacement
-        float finalVelocity = initialVelocity + accelerationAlongRamp * time;
+        // Calculate the net force acting on the block
+        Vector3 netForce = GetComponent<Rigidbody>().mass * acceleration;
 
-        // Set velocity of block in direction of inclined plane
-        Vector3 velocity = new Vector3(finalVelocity * Mathf.Cos(rampAngleRad), 0f, finalVelocity * Mathf.Sin(rampAngleRad));
-        rb.velocity = velocity;
+        // Calculate the frictional force acting on the block
+        Vector3 frictionForceVector = -frictionForce * currentVelocityAlongRamp.normalized;
+
+        // Apply the net force and frictional force to the block
+        GetComponent<Rigidbody>().AddForce(netForce + frictionForceVector, ForceMode.Force);
     }
 }
